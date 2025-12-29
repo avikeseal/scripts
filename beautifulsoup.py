035c16e2acfa897e5050162fb70729c0499131bf
avikeseal <avikeseal@gmail.com>
2025-12-28 21:04:45 -0500

    html parsing with beautifulsoup

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -3,8 +3,11 @@ import requests
 from bs4 import BeautifulSoup
 
 #get a simple request from web page 
-url = 'https://x.com'
+url = 'https://api.github.com'
 response = requests.get(url)
 
+soup = BeautifulSoup(response.content, 'html.parser')
+
 #check to see if it works:
+print('soup.title)
 print(f'status code: {response.status_code}') #output should be 200
