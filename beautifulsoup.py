10d6caf50b3c0444adbb76c83c14d8557a19c74c
avikeseal <avikeseal@gmail.com>
2025-12-28 21:06:49 -0500

    missing quotation mark fixed at line 12

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -9,5 +9,5 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 #check to see if it works:
-print('soup.title)
+print('soup.title')
 print(f'status code: {response.status_code}') #output should be 200
