d56e3716b67c1d9112607eb785eb86c93dbaa5b5
avikeseal <avikeseal@gmail.com>
2026-01-10 00:09:00 -0500

    simple script that returns the ip addresses within the network we are identifying

---
+++ slaughterhouse/pingsweep.sh
@@ -0,0 +1,9 @@
+#!usr/bin/env bash
+
+#in sequence of 1 thru 254 we use 'do' to perform ping
+for ip in `seq 1 254`; do
+
+#$1 asks for user input which should be an IP address
+ping -c 1 $1.$ip | grep '64 bytes' | cut -d " " -f 4 | tr -d ':' &
+done
+
